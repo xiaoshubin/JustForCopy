b6989e56b94f645c6ce18c088a81094ace560c50
xiaoshubin <495303648@qq.com>
2021-07-13 22:57:43 +0800

    评分控件:可点击，可滚动，可设置步长0.1

--- app/src/main/java/com/smallcake/temp/fragment/ListFragment.kt
+++ app/src/main/java/com/smallcake/temp/fragment/ListFragment.kt
@@ -4,12 +4,20 @@ import android.os.Bundle
 import android.view.View
 import com.smallcake.temp.base.BaseBindFragment
 import com.smallcake.temp.databinding.FragmentListBinding
+import com.willy.ratingbar.BaseRatingBar
 
 class ListFragment: BaseBindFragment<FragmentListBinding>() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        onEvent()
 
+    }
+
+    private fun onEvent() {
+        bind.scaleRatingBar.setOnRatingChangeListener{ baseRatingBar: BaseRatingBar, rating: Float, fromUser: Boolean ->
+            bind.tvScaleRatingBarDesc.text = "$rating 分"
+        }
     }
 
 
