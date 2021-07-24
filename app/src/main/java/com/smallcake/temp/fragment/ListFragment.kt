3dd7d05aff0765fa44de398a6e729520a37816ea
xiaoshubin <495303648@qq.com>
2021-07-24 22:37:01 +0800

    原生时间选择器

--- app/src/main/java/com/smallcake/temp/fragment/ListFragment.kt
+++ app/src/main/java/com/smallcake/temp/fragment/ListFragment.kt
@@ -1,32 +1,42 @@
 package com.smallcake.temp.fragment
 
 import android.annotation.SuppressLint
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
+import android.content.DialogInterface
 import android.graphics.Color
 import android.os.Bundle
+import android.os.Message
 import android.view.MotionEvent
 import android.view.View
+import android.widget.AdapterView
+import android.widget.SimpleAdapter
+import android.widget.Spinner
 import com.bigkoo.pickerview.builder.TimePickerBuilder
-import com.smallcake.smallutils.KeyboardUtils
+import com.smallcake.smallutils.Screen
+import com.smallcake.smallutils.SpannableStringUtils
 import com.smallcake.smallutils.TimeUtils
+import com.smallcake.temp.R
 import com.smallcake.temp.base.BaseBindFragment
 import com.smallcake.temp.databinding.FragmentListBinding
+import com.smallcake.temp.utils.showToast
 import com.willy.ratingbar.BaseRatingBar
 import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.collections.HashMap
 
 class ListFragment: BaseBindFragment<FragmentListBinding>() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initView()
         onEvent()
 
     }
-
-    @SuppressLint("ClickableViewAccessibility")
-    private fun onEvent() {
-        //评分控件
-        bind.scaleRatingBar.setOnRatingChangeListener{ baseRatingBar: BaseRatingBar, rating: Float, fromUser: Boolean ->
-            bind.tvScaleRatingBarDesc.text = "$rating 分"
-        }
+    fun text() {
+        val res = intArrayOf(1, 2, 3)
+    }
+    private fun initView() {
         //声音波纹控件
         bind.lineVoice.apply {
             duration = 150
@@ -49,8 +59,53 @@ class ListFragment: BaseBindFragment<FragmentListBinding>() {
             addBody(48)
             addBody(32)
         }
-        bind.audioRecordBtn.setOnTouchListener{ v, event->
+        //原生下拉控件Spinner
+        val spinnerDatas = getSpinnerDatas()
+        val spinnerAdapter = SimpleAdapter(activity, spinnerDatas,R.layout._xpopup_adapter_text,
+            arrayOf("img","tv"),
+            intArrayOf(R.id.iv_image,R.id.tv_text)
+        )
+        //下拉框遮挡住了Spinner的显示框,设置android:overlapAnchor=”false”
+        bind.spinner1.adapter = spinnerAdapter
+        bind.spinner1.onItemSelectedListener = object :AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                val item = spinnerDatas[position]
+                showToast(item.get("tv").toString())
 
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+            }
+
+        }
+
+    }
+    private fun getSpinnerDatas():ArrayList<Map<String,Any>>{
+        val data = ArrayList<Map<String,Any>>()
+        val map = HashMap<String,Any>()
+        map["img"] = R.mipmap.icon_selected_tab1
+        map["tv"] = "死神"
+        data.add(map)
+        val map2 = HashMap<String,Any>()
+        map2["img"] = R.mipmap.icon_selected_tab2
+        map2["tv"] = "海贼王"
+        data.add(map2)
+        return data
+    }
+
+    @SuppressLint("ClickableViewAccessibility", "SetTextI18n")
+    private fun onEvent() {
+        //评分控件
+        bind.scaleRatingBar.setOnRatingChangeListener{ baseRatingBar: BaseRatingBar, rating: Float, fromUser: Boolean ->
+            bind.tvScaleRatingBarDesc.text = "$rating 分"
+        }
+        //声音波纹控件
+        bind.audioRecordBtn.setOnTouchListener{ v, event->
             if (event.action == MotionEvent.ACTION_DOWN) {
                 bind.lineVoice.start()
 
@@ -66,8 +121,22 @@ class ListFragment: BaseBindFragment<FragmentListBinding>() {
         bind.tvYmdhm.setOnClickListener{
             showTimePicker()
         }
+        bind.tvYmd.setOnClickListener{
+            showYMD{year,month,day->
+                bind.tvYmd.text = "$year-${month+1}-$day"
+            }
+        }
+        bind.tvHm.setOnClickListener{
+            showHM{h,m->
+                bind.tvHm.text = "${h}时${m}分"
+
+            }
+        }
     }
 
+    /**
+     * 第三方年月日时分秒选择器
+     */
     private fun showTimePicker() {
         val showTypes = booleanArrayOf(true,true,true,true,true,false)
         val types = BooleanArray(6)
@@ -89,8 +158,60 @@ class ListFragment: BaseBindFragment<FragmentListBinding>() {
         picker.show(true)
     }
 
+    /**
+     * 原生年月日选择器
+     */
+    fun showYMD( listener: (Int,Int,Int)->Unit) {
+        val ca = Calendar.getInstance()
+        val caMax = Calendar.getInstance()
+        val mYear = ca[Calendar.YEAR]
+        val mMonth = ca[Calendar.MONTH]
+        val mDay = ca[Calendar.DAY_OF_MONTH]
+        val datePickerDialog = DatePickerDialog(requireContext(),R.style.dialog_date,
+            { view, year, month, dayOfMonth -> listener.invoke(year,month,dayOfMonth)},
+            mYear, mMonth, mDay)
+        val msg = Message()
+        msg.what = DialogInterface.BUTTON_POSITIVE
+        val confirmText = SpannableStringUtils.getBuilder("确定").setForegroundColor(Color.RED).create()
+        datePickerDialog.setButton(DialogInterface.BUTTON_POSITIVE,confirmText) { dialog, which ->
+            listener.invoke(
+                datePickerDialog.datePicker.year,
+                datePickerDialog.datePicker.month,
+                datePickerDialog.datePicker.dayOfMonth
+            )
+        }
+        val cancleText = SpannableStringUtils.getBuilder("取消").setForegroundColor(Color.RED).create()
+        datePickerDialog.setButton(DialogInterface.BUTTON_NEGATIVE,cancleText) { dialog, which ->
+
+        }
+        val datePicker = datePickerDialog.datePicker
+        //范围控制,最大能选两年前的月份
+        caMax.add(Calendar.YEAR, 1)
+        datePicker.minDate = ca.timeInMillis //最小为当前
+        datePicker.maxDate = caMax.timeInMillis //最大时间为当前年月
+        //范围控制
+        datePickerDialog.show()
+    }
+
+    /**
+     * 显示时分
+     * @param activity Context?
+     * @param listener Function4<DatePicker?, Int, Int, Int, Unit>
+     */
+    fun showHM( listener: (Int, Int) -> Unit) {
+        val ca = Calendar.getInstance()
+        val mHour = ca[Calendar.HOUR]
+        val mMinute = ca[Calendar.MINUTE]
+        TimePickerDialog(activity,0,
+            { _, hourOfDay, minute ->
+                listener.invoke(hourOfDay, minute)
+            },mHour,mMinute,true).show()
+    }
+
 
 }
+
+
 
 /**
  * 范围设定 ：从当前时间 到 一年后的当前时间
