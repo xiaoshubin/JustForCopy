3db6afa0696593d40b09b1613efb45d4ad82563a
xiaoshubin <495303648@qq.com>
2021-08-14 12:00:36 +0800

    优化展开收起自定义文本控件行数问题

--- app/src/main/java/com/smallcake/temp/ui/TextViewActivity.kt
+++ app/src/main/java/com/smallcake/temp/ui/TextViewActivity.kt
@@ -1,13 +1,16 @@
 package com.smallcake.temp.ui
 
+
 import android.os.Bundle
 import com.smallcake.smallutils.text.NavigationBar
 import com.smallcake.temp.base.BaseBindActivity
+import com.smallcake.temp.databinding.ActivityTextViewBinding
+
 
 /**
  * https://github.com/Cmahjong/ExpandTextView
  */
-class TextViewActivity : BaseBindActivity<com.smallcake.temp.databinding.ActivityTextViewBinding>() {
+class TextViewActivity : BaseBindActivity<ActivityTextViewBinding>() {
 
 
     override fun onCreate(savedInstanceState: Bundle?, bar: NavigationBar) {
@@ -16,36 +19,6 @@ class TextViewActivity : BaseBindActivity<com.smallcake.temp.databinding.Activit
     }
 
     private fun onEvent() {
-        bind.tvAdd.setOnClickListener{
-            var text = bind.tvAutosize.text.toString()
-            text +=text
-            bind.tvAutosize.text=text
-        }
-        bind.tvReset.setOnClickListener{
-            bind.tvAutosize.text="大小"
-        }
-        val str = "雷军用实际行动证明：一个不会蹭热度的程序员不会是一个好的CEO。程序员在大家刻板的印象里就是格子衫、油腻、头发稀疏的形象，然而事实并非如此。绝大多数的程序员都很聪明，更有一些程序员已经成了精，不仅编程厉害，营销方面也是不输任何营销大师，最典型的代表就是雷军。"
-        //设置最大显示行数
-//        bind.expandTextView.setText(str,false,object :Callback{
-//            override fun onExpand() {
-//            }
-//
-//            override fun onCollapse() {
-//            }
-//
-//            override fun onLoss() {
-//            }
-//
-//            override fun onExpandClick() {
-//                bind.expandTextView.setChanged(true)
-//            }
-//
-//            override fun onCollapseClick() {
-//                bind.expandTextView.setChanged(false)
-//            }
-//
-//        })
-
 
 
     }
--- app/src/main/java/com/smallcake/temp/weight/ExpandTextView.kt
+++ app/src/main/java/com/smallcake/temp/weight/ExpandTextView.kt
@@ -16,55 +16,39 @@ import androidx.appcompat.widget.AppCompatTextView
 /**
  * 参考：https://github.com/timqi/CollapsibleTextView
  * 参考：https://github.com/Cmahjong/ExpandTextView
- *
  * 收起展开文本控件
- * @property expandState Boolean
- * @property mCallback Callback?
- * @property mText String?
- * @property maxLineCount Int
- * @property ellipsizeText String
- * @property expandText String
- * @property expandTextColor Int
- * @property collapseText String
- * @property collapseTextColor Int
- * @property collapseEnable Boolean
- * @property underlineEnable Boolean
- * @property marginStartPX Int
- * @property marginEndPX Int
- * @property clickEnable Boolean
- *
  */
 class ExpandTextView : AppCompatTextView {
     /**展开状态 true：展开，false：收起 */
     var expandState: Boolean = false
+
     /** 源文字内容 */
     var mText: String? = ""
+
     /** 最多展示的行数 */
-    var maxLineCount = 2
+    var maxLineCount = 3
+
     /** 省略文字 */
     var ellipsizeText = "..."
+
     /** 展开文案文字 */
-    var expandText = "查看全文"
+    var expandText = "全文"
+
     /** 展开文案文字颜色 */
     var expandTextColor: Int = Color.parseColor("#1C7FFD")
+
     /** 收起文案文字 */
-    var collapseText = "收起来"
+    var collapseText = "收起"
+
     /** 收起文案文字颜色 */
-    var collapseTextColor: Int = Color.parseColor("#999999")
-    /**是否支持收起功能*/
-    var collapseEnable = true
+    var collapseTextColor: Int = Color.parseColor("#1C7FFD")
+
     /** 是否添加下划线 */
     var underlineEnable = false
-    var marginStartPX = 0//marginStart
-    var marginEndPX = 0//marginStart
-    /** 是否应该初始化布局 */
-    private var mShouldInitLayout = true
-    private var mSuffixTrigger = true//是否只有点击后缀才伸缩
-    private var mCustomClickListener: OnClickListener? = null
 
-    override fun setOnClickListener(l: OnClickListener?) {
-        mCustomClickListener = l
-    }
+    /** 是否仅仅点击后缀才伸缩 true:点击后缀才伸缩，点击全文收缩 */
+    private var mSuffixTrigger = true
+
     constructor(context: Context) : super(context) {
         initTextView()
     }
@@ -73,7 +57,11 @@ class ExpandTextView : AppCompatTextView {
         initTextView()
     }
 
-    constructor(context: Context, attributes: AttributeSet, defStyleAttr: Int) : super(context, attributes, defStyleAttr) {
+    constructor(context: Context, attributes: AttributeSet, defStyleAttr: Int) : super(
+        context,
+        attributes,
+        defStyleAttr
+    ) {
         initTextView()
     }
 
@@ -87,10 +75,8 @@ class ExpandTextView : AppCompatTextView {
         OnClickListener { v ->
             if (!mSuffixTrigger) {
                 expandState = !expandState
-                applyState(expandState)
+                applyState()
             }
-            mCustomClickListener?.onClick(v)
-
         }
 
     @SuppressLint("DrawAllocation")
@@ -99,11 +85,10 @@ class ExpandTextView : AppCompatTextView {
         if (measuredWidth == 0) {
             return
         }
-        if ((mText?.length?:0) == 0) {
+        if ((mText?.length ?: 0) == 0) {
             return
         }
-        val canUseWidth =
-            resources.displayMetrics.widthPixels - paddingLeft - paddingRight - marginStartPX - marginEndPX
+        val canUseWidth = resources.displayMetrics.widthPixels - paddingLeft - paddingRight
         //StaticLayout对象
         val sl = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             StaticLayout.Builder.obtain(
@@ -126,43 +111,33 @@ class ExpandTextView : AppCompatTextView {
                 true
             )
         }
-        var result:SpannableString?=null
+        val result: SpannableString?
         // 总计行数
         var lineCount = sl.lineCount
         //总行数大于最大行数
         if (lineCount > maxLineCount) {
             if (expandState) {
-                //是否支持收起功能
-                if (collapseEnable) {
-                    // 收起文案和源文字组成的新的文字
-                    val newEndLineText = mText + collapseText
-                    //收起文案和源文字组成的新的文字
-                    result = SpannableString(newEndLineText).apply {
-                            //给收起设成监听
-                                setSpan(object : ClickableSpan() {
-                                        override fun onClick(widget: View) {
-                                            setChanged(false)
-                                        }
-                                        override fun updateDrawState(ds: TextPaint) {
-                                            super.updateDrawState(ds)
-                                            ds.isUnderlineText = false
-                                        }
+                // 收起文案和源文字组成的新的文字
+                val newEndLineText = mText + collapseText
+                //收起文案和源文字组成的新的文字
+                result = SpannableString(newEndLineText).apply {
+                    //给收起设成监听
+                    setSpan(
+                        mClickSpanListener,
+                        newEndLineText.length - collapseText.length,
+                        newEndLineText.length,
+                        Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+                    )
 
-                                    },
-                                    newEndLineText.length - collapseText.length,
-                                    newEndLineText.length,
-                                    Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
-                                )
-
-                            //给收起设成蓝色
-                            setSpan(
-                                ForegroundColorSpan(collapseTextColor),
-                                newEndLineText.length - collapseText.length,
-                                newEndLineText.length,
-                                Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
-                            )
-                        }
+                    //给收起设成蓝色
+                    setSpan(
+                        ForegroundColorSpan(collapseTextColor),
+                        newEndLineText.length - collapseText.length,
+                        newEndLineText.length,
+                        Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+                    )
                 }
+
             } else {
 
                 lineCount = maxLineCount
@@ -174,9 +149,9 @@ class ExpandTextView : AppCompatTextView {
                 var lineText = mText?.substring(start, end) ?: ""
                 // 将第最后一行最后的文字替换为 ellipsizeText和expandText
                 //如果有换行符的话，会出现收齐状态显示未占满全文状态，那么先判断收齐状态的情况下是否有换行符，然后文字内容加上省略符号是否超过可用宽度
-                lineText = lineText.replace("\r\n", "",true)
-                lineText = lineText.replace("\n", "",true)
-                var newEndLineText: String
+                lineText = lineText.replace("\r\n", "", true)
+                lineText = lineText.replace("\n", "", true)
+                val newEndLineText: String
                 if (paint.measureText(lineText + ellipsizeText + expandText) > canUseWidth) {
                     var endIndex = 0
                     for (i in lineText.length - 1 downTo 0) {
@@ -193,25 +168,19 @@ class ExpandTextView : AppCompatTextView {
                         endIndex
                     ) + ellipsizeText + expandText
                 } else {
-                    newEndLineText = (mText?.substring(0, start) ?: "") +lineText + ellipsizeText + expandText
+                    newEndLineText =
+                        (mText?.substring(0, start) ?: "") + lineText + ellipsizeText + expandText
                 }
 
                 //全部文字
                 result = SpannableString(newEndLineText).apply {
-                        //给查看全部设成监听
-                        setSpan(object : ClickableSpan() {
-                                override fun onClick(widget: View) {
-                                    setChanged(true)
-                                }
-                                override fun updateDrawState(ds: TextPaint) {
-                                    super.updateDrawState(ds)
-                                    ds.isUnderlineText = false
-                                }
-                            },
-                            newEndLineText.length - expandText.length,
-                            newEndLineText.length,
-                            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
-                        )
+                    //给查看全部设成监听
+                    setSpan(
+                        mClickSpanListener,
+                        newEndLineText.length - expandText.length,
+                        newEndLineText.length,
+                        Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+                    )
 
                     //给查看全部设成颜色
                     setSpan(
@@ -228,41 +197,47 @@ class ExpandTextView : AppCompatTextView {
         }
         // 重新计算高度
         var lineHeight = 0f
-        for (i in 0 until lineCount) {
-            lineHeight +=(paint.fontMetrics.bottom - paint.fontMetrics.top)* lineSpacingMultiplier
+        for (i in 0 .. lineCount) {
+            lineHeight += (paint.fontMetrics.bottom - paint.fontMetrics.top) * lineSpacingMultiplier
         }
         lineHeight += paddingTop + paddingBottom
-        setMeasuredDimension(measuredWidth, lineHeight.toInt()+1)
+        setMeasuredDimension(measuredWidth, lineHeight.toInt() + 1)
         text = result
     }
 
     override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
         super.onLayout(changed, left, top, right, bottom)
-        if (mShouldInitLayout && lineCount > maxLineCount) {
-            mShouldInitLayout = false
-            applyState(expandState)
+        if (lineCount > maxLineCount) {
+            applyState()
         }
     }
-    private fun applyState(expanded: Boolean) {
+
+    private fun applyState() {
         if (TextUtils.isEmpty(mText)) return
-        var note = mText!!
-        val suffix: String
-        if (expanded) {
+        var note:String
+        val suffix: String//文字后缀：如全文|收起
+        if (expandState) {
+            note = mText!!
             suffix = collapseText
         } else {
+            note = mText+ellipsizeText
+            suffix = expandText
             if (maxLineCount - 1 < 0) {
                 throw RuntimeException("CollapsedLines must equal or greater than 1")
             }
             val lineEnd = layout.getLineEnd(maxLineCount - 1)
-            suffix = expandText
+
+
             val newEnd = lineEnd - suffix.length - 1
+
             var end = if (newEnd > 0) newEnd else lineEnd
             val paint = paint
-            val maxWidth: Int = maxLineCount * (measuredWidth - paddingLeft - paddingRight)
+            val maxWidth: Int =
+                maxLineCount * (measuredWidth - paddingLeft - paddingRight)
             while (paint.measureText(note.substring(0, end) + suffix) > maxWidth) end--
             note = note.substring(0, end)
         }
-        val str = SpannableString(note + suffix)
+        val str = SpannableString(note +suffix)
         if (mSuffixTrigger) {
             str.setSpan(
                 mClickSpanListener,
@@ -270,13 +245,13 @@ class ExpandTextView : AppCompatTextView {
                 note.length + suffix.length,
                 SpannableString.SPAN_EXCLUSIVE_EXCLUSIVE
             )
-            str.setSpan(
-                ForegroundColorSpan(if (expanded)collapseTextColor else expandTextColor),
-                note.length,
-                note.length + suffix.length,
-                SpannableString.SPAN_EXCLUSIVE_EXCLUSIVE
-            )
         }
+        str.setSpan(
+            ForegroundColorSpan(if (expandState) collapseTextColor else expandTextColor),
+            note.length,
+            note.length + suffix.length,
+            SpannableString.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
         post { text = str }
     }
 
@@ -284,35 +259,16 @@ class ExpandTextView : AppCompatTextView {
         override fun onClick(widget: View) {
             if (mSuffixTrigger) {
                 expandState = !expandState
-                applyState(expandState)
+                applyState()
             }
         }
 
         override fun updateDrawState(ds: TextPaint) {
             super.updateDrawState(ds)
-            ds.isUnderlineText = false
+            ds.isUnderlineText = underlineEnable
         }
     }
 
-    /**
-     * 设置要显示的文字以及状态
-     * @param text
-     * @param expanded true：展开，false：收起
-     * @param callback
-     */
-    fun setText(text: String?, expanded: Boolean) {
-        mText = text
-        expandState = expanded
-        invalidate()
-    }
 
-    /**
-     * 展开收起状态变化
-     * @param expanded
-     */
-    fun setChanged(expanded: Boolean) {
-        expandState = expanded
-        requestLayout()
-    }
 }
 
