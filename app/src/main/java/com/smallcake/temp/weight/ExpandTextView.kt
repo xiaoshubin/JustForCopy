50211a234d6a94f1d86d32c6d42d84ec475e9cc4
xiaoshubin <495303648@qq.com>
2021-08-14 15:37:52 +0800

    优化展开收起自定义文本控件初始收起状态

--- app/src/main/java/com/smallcake/temp/weight/ExpandTextView.kt
+++ app/src/main/java/com/smallcake/temp/weight/ExpandTextView.kt
@@ -1,10 +1,10 @@
 package com.smallcake.temp.weight
 
-import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Color
-import android.os.Build
-import android.text.*
+import android.text.SpannableString
+import android.text.TextPaint
+import android.text.TextUtils
 import android.text.method.LinkMovementMethod
 import android.text.style.ClickableSpan
 import android.text.style.ForegroundColorSpan
@@ -12,6 +12,7 @@ import android.util.AttributeSet
 import android.view.View
 import android.view.View.OnClickListener
 import androidx.appcompat.widget.AppCompatTextView
+import com.smallcake.temp.utils.L
 
 /**
  * 参考：https://github.com/timqi/CollapsibleTextView
@@ -49,6 +50,8 @@ class ExpandTextView : AppCompatTextView {
     /** 是否仅仅点击后缀才伸缩 true:点击后缀才伸缩，点击全文收缩 */
     private var mSuffixTrigger = true
 
+    private var mShouldInitLayout = true
+
     constructor(context: Context) : super(context) {
         initTextView()
     }
@@ -79,177 +82,56 @@ class ExpandTextView : AppCompatTextView {
             }
         }
 
-    @SuppressLint("DrawAllocation")
-    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
-        if (measuredWidth == 0) {
-            return
-        }
-        if ((mText?.length ?: 0) == 0) {
-            return
-        }
-        val canUseWidth = resources.displayMetrics.widthPixels - paddingLeft - paddingRight
-        //StaticLayout对象
-        val sl = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            StaticLayout.Builder.obtain(
-                mText ?: "",
-                0,
-                mText?.length ?: 0,
-                paint,
-                canUseWidth
-            ).apply {
-                setAlignment(Layout.Alignment.ALIGN_CENTER)
-            }.build()
-        } else {
-            StaticLayout(
-                mText,
-                paint,
-                canUseWidth,
-                Layout.Alignment.ALIGN_CENTER,
-                1f,
-                0f,
-                true
-            )
-        }
-        val result: SpannableString?
-        // 总计行数
-        var lineCount = sl.lineCount
-        //总行数大于最大行数
-        if (lineCount > maxLineCount) {
-            if (expandState) {
-                // 收起文案和源文字组成的新的文字
-                val newEndLineText = mText + collapseText
-                //收起文案和源文字组成的新的文字
-                result = SpannableString(newEndLineText).apply {
-                    //给收起设成监听
-                    setSpan(
-                        mClickSpanListener,
-                        newEndLineText.length - collapseText.length,
-                        newEndLineText.length,
-                        Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
-                    )
-
-                    //给收起设成蓝色
-                    setSpan(
-                        ForegroundColorSpan(collapseTextColor),
-                        newEndLineText.length - collapseText.length,
-                        newEndLineText.length,
-                        Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
-                    )
-                }
-
-            } else {
-
-                lineCount = maxLineCount
-                // 省略文字和展开文案的宽度
-                val dotWidth = paint.measureText(ellipsizeText + expandText)
-                // 找出显示最后一行的文字
-                val start = sl.getLineStart(lineCount - 1)
-                val end = sl.getLineEnd(lineCount - 1)
-                var lineText = mText?.substring(start, end) ?: ""
-                // 将第最后一行最后的文字替换为 ellipsizeText和expandText
-                //如果有换行符的话，会出现收齐状态显示未占满全文状态，那么先判断收齐状态的情况下是否有换行符，然后文字内容加上省略符号是否超过可用宽度
-                lineText = lineText.replace("\r\n", "", true)
-                lineText = lineText.replace("\n", "", true)
-                val newEndLineText: String
-                if (paint.measureText(lineText + ellipsizeText + expandText) > canUseWidth) {
-                    var endIndex = 0
-                    for (i in lineText.length - 1 downTo 0) {
-                        val str = lineText.substring(i, lineText.length)
-                        // 找出文字宽度大于 ellipsizeText 的字符
-                        if (paint.measureText(str) >= dotWidth) {
-                            endIndex = i
-                            break
-                        }
-                    }
-                    // 新的文字
-                    newEndLineText = (mText?.substring(0, start) ?: "") + lineText.substring(
-                        0,
-                        endIndex
-                    ) + ellipsizeText + expandText
-                } else {
-                    newEndLineText =
-                        (mText?.substring(0, start) ?: "") + lineText + ellipsizeText + expandText
-                }
-
-                //全部文字
-                result = SpannableString(newEndLineText).apply {
-                    //给查看全部设成监听
-                    setSpan(
-                        mClickSpanListener,
-                        newEndLineText.length - expandText.length,
-                        newEndLineText.length,
-                        Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
-                    )
-
-                    //给查看全部设成颜色
-                    setSpan(
-                        ForegroundColorSpan(expandTextColor),
-                        newEndLineText.length - expandText.length,
-                        newEndLineText.length,
-                        Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
-                    )
-                }
-
-            }
-        } else {
-            result = SpannableString(mText)
-        }
-        // 重新计算高度
-        var lineHeight = 0f
-        for (i in 0 .. if (expandState)lineCount else lineCount-1) {
-            lineHeight += (paint.fontMetrics.bottom - paint.fontMetrics.top) * lineSpacingMultiplier
-        }
-        lineHeight += paddingTop + paddingBottom
-        setMeasuredDimension(measuredWidth, lineHeight.toInt() + 1)
-        text = result
-    }
-
     override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
         super.onLayout(changed, left, top, right, bottom)
-        if (lineCount > maxLineCount) {
+        if (mShouldInitLayout&&lineCount > maxLineCount) {
+            mShouldInitLayout=false
             applyState()
         }
     }
-
+    private var index=0
     private fun applyState() {
         if (TextUtils.isEmpty(mText)) return
-        var note:String
+        var note:String = mText!!
         val suffix: String//文字后缀：如全文|收起
         if (expandState) {
-            note = mText!!
             suffix = collapseText
         } else {
-            note = mText+ellipsizeText
             suffix = expandText
             if (maxLineCount - 1 < 0) {
                 throw RuntimeException("CollapsedLines must equal or greater than 1")
             }
-            val lineEnd = layout.getLineEnd(maxLineCount - 1)
 
+            var lineEnd = layout.getLineEnd(maxLineCount -1)
+            //第一次因为计算了追加后缀的长度，所以要减去
+            if (index==0){
+                lineEnd -= expandText.length
+                index++
+            }
 
-            val newEnd = lineEnd - suffix.length - 1
+            val newEnd = lineEnd - suffix.length - 1+ellipsizeText.length
 
-            var end = if (newEnd > 0) newEnd else lineEnd
+            var end = lineEnd.coerceAtMost(newEnd)
             val paint = paint
             val maxWidth: Int =
                 maxLineCount * (measuredWidth - paddingLeft - paddingRight)
             while (paint.measureText(note.substring(0, end) + suffix) > maxWidth) end--
+            L.e("lineEnd=$lineEnd newEnd=$newEnd end=$end")
             note = note.substring(0, end)
         }
-        val str = SpannableString(note +suffix)
+        val str = SpannableString(note +(if (expandState)suffix else(ellipsizeText+suffix)))
         if (mSuffixTrigger) {
             str.setSpan(
                 mClickSpanListener,
-                note.length,
-                note.length + suffix.length,
+                if (expandState)note.length else note.length+ellipsizeText.length,
+                if (expandState)(note.length + suffix.length)else (note.length + suffix.length+ellipsizeText.length),
                 SpannableString.SPAN_EXCLUSIVE_EXCLUSIVE
             )
         }
         str.setSpan(
             ForegroundColorSpan(if (expandState) collapseTextColor else expandTextColor),
-            note.length,
-            note.length + suffix.length,
+            if (expandState)note.length else note.length+ellipsizeText.length,
+            if (expandState)(note.length + suffix.length)else (note.length + suffix.length+ellipsizeText.length),
             SpannableString.SPAN_EXCLUSIVE_EXCLUSIVE
         )
         post { text = str }
