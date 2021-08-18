1656cb1e171fae1bfc165bdcee15f29f655c5e01
xiaoshubin <495303648@qq.com>
2021-08-18 21:43:05 +0800

    转盘抽奖优化

--- app/src/main/java/com/smallcake/temp/weight/LuckPan.kt
+++ app/src/main/java/com/smallcake/temp/weight/LuckPan.kt
@@ -6,6 +6,9 @@ import android.animation.ValueAnimator
 import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.*
+import android.os.Handler
+import android.os.Looper
+import android.text.TextPaint
 import android.util.AttributeSet
 import android.view.MotionEvent
 import android.view.View
@@ -21,6 +24,7 @@ import kotlin.math.roundToInt
  * https://github.com/Nipuream/LuckPan/blob/master/app/src/main/java/com/hr/nipuream/luckpan/view/RotatePan.java
  * https://blog.csdn.net/YanghuiNipurean/article/details/52251107
  * http://inloop.github.io/interpolator/
+ * 【文字绘制】https://www.cnblogs.com/duoshou/articles/9071237.html
  *
  * 注意：大小最好在200dp-300dp之间
  */
@@ -28,7 +32,7 @@ open class LuckPan: View {
 
 
     private val panNum       = 6               //奖盘数量
-    private var initAngle    = 0f            //初始化开始绘制角度（-60指向最后一块的中心位置，如果每块的角度为60）
+    private var initAngle    = -120f           //初始化开始绘制角度（-60指向最后一块的中心位置，如果每块的角度为60）
     private val panRadius    = 360f / panNum   //圆盘根据盘数量平分后的角度（60）
     private val turnTime     = 3000L           //旋转所需要的时间
     private var panSize      = 200             //抽奖盘的大小，默认200           注意：设置宽高后会重新测量
@@ -40,27 +44,23 @@ open class LuckPan: View {
     private val lampMargin   = 8f                                   //灯泡距离边框距离
     private val lampPadding  = 16f                                  //灯泡距离抽奖区块距离
     private val lampSpace    = lampSize*2+lampMargin+lampPadding    //灯泡位置占用
-    private val lampRatate   = 360f/lampNum                         //灯泡之间的角度
+    private val lampAngle   = 360f/lampNum                          //灯泡之间的角度
 
     private val bg2Size      = 8f                                   //第二层背景宽度
 
 
-    private val paintBg   = Paint(Paint.ANTI_ALIAS_FLAG)      //背景大圆盘（黄色）
-    private val paintBg2  = Paint(Paint.ANTI_ALIAS_FLAG)      //背景大圆盘2（橙色）
-    private val paintLamp = Paint(Paint.ANTI_ALIAS_FLAG)      //背景大圆盘灯泡（黄白灯）
-    private val paintArc  = Paint(Paint.ANTI_ALIAS_FLAG)      //奖品区块（红白色）
-    private val paintText = Paint(Paint.ANTI_ALIAS_FLAG)      //奖品区块文字（黄色）
+    private val paintBg      = Paint(Paint.ANTI_ALIAS_FLAG)          //背景大圆盘（黄色）
+    private val paintBg2     = Paint(Paint.ANTI_ALIAS_FLAG)          //背景大圆盘2（橙色）
+    private val paintLamp    = Paint(Paint.ANTI_ALIAS_FLAG)          //背景大圆盘灯泡（黄白灯）
+    private val paintArc     = Paint(Paint.ANTI_ALIAS_FLAG)          //奖品区块（红白色）
+    private val paintText    = Paint(Paint.ANTI_ALIAS_FLAG)          //奖品区块文字（黄色）
+    private val paintBtnText = TextPaint(Paint.ANTI_ALIAS_FLAG)      //中心按钮文字（白色）
 
     private val isOpenLamp = true                             //是否需要打开了霓虹灯交替
     private var isLight = false                               //灯泡是否点亮
 
     private var canClickCenter = true                         //是否可以点击中心区域，点击后禁用，避免转动中再次点击触发转盘
     private var isTouchDown = false                           //是否按下了中心按钮
-
-
-
-
-
 
 
     constructor(context: Context?):super(context){init()}
@@ -89,8 +89,13 @@ open class LuckPan: View {
         }
         paintText.apply {
             color = Color.parseColor("#FFBE04")
-            textSize = DpUtils.dp2pxFloat(14f)
-            letterSpacing = 0.5f
+            textSize = DpUtils.dp2pxFloat(18f)
+            letterSpacing = 0.2f
+            isFakeBoldText = true
+        }
+        paintBtnText.apply {
+            color = Color.WHITE
+            textSize = DpUtils.dp2pxFloat(22f)
             isFakeBoldText = true
         }
         isClickable = true
@@ -98,11 +103,11 @@ open class LuckPan: View {
         if (isOpenLamp)mHandler.sendEmptyMessage(0)
 
     }
-    private val mHandler = android.os.Handler{
-         isLight = !isLight
-         it.target.sendEmptyMessageDelayed(0,800)
-         postInvalidate()
-         false
+    private val mHandler = Handler(Looper.getMainLooper()){
+        isLight = !isLight
+        it.target.sendEmptyMessageDelayed(0,800)
+        postInvalidate()
+        false
     }
 
     private fun initSize() {
@@ -123,7 +128,7 @@ open class LuckPan: View {
         //3.绘制奖品扇形区块和奖品名称
         drawLuckArc(canvas)
         //4.绘制向上的三角形
-        drawTrangle(canvas)
+        drawTriangle(canvas)
         //5.绘制中间圆圈按钮
         drawLuckCenter(canvas)
     }
@@ -139,7 +144,7 @@ open class LuckPan: View {
             paintLamp.color = Color.parseColor(if (isLight)"#ffffff" else "#F53030")
             paintLamp.style = if (i%2==0) Paint.Style.FILL else Paint.Style.STROKE
             canvas.drawCircle(cx,cy,if (i%2==0) lampSize else lampSize/3*2, paintLamp)
-            canvas.rotate(lampRatate, width / 2f, height / 2f)
+            canvas.rotate(lampAngle, width / 2f, height / 2f)
         }
     }
 
@@ -156,10 +161,21 @@ open class LuckPan: View {
         for (i in 0 until panNum) {
             paintArc.color = Color.parseColor(if (i%2==0)"#F53030" else "#ffffff")
             canvas.drawArc(rectF, angle, panRadius, true, paintArc)
-            drawText(angle,"${ (i+1)}等奖",canvas,rectF)
+
+            paintText.color = Color.parseColor(if (i%2==0)"#ffffff" else "#F53030")
+            drawText(angle,tranLunkName(i),canvas,rectF)
             angle += panRadius
         }
-
+    }
+    private fun tranLunkName(position:Int):String{
+        return when(position){
+            0 -> "一等奖"
+            1 -> "二等奖"
+            2 -> "三等奖"
+            3 -> "四等奖"
+            4 -> "五等奖"
+            else -> "谢谢参与"
+        }
     }
 
     /**
@@ -173,13 +189,14 @@ open class LuckPan: View {
         val hOffset = (panSize * Math.PI / panNum / 2 - textWidth/2-8f ).toFloat()
         //圆弧的垂直偏移
         val vOffset = (panSize / 12).toFloat()
+
         mCanvas.drawTextOnPath(string, path, hOffset, vOffset, paintText)
     }
 
     /**
      * 绘制三角形箭头
      */
-    private fun drawTrangle(canvas: Canvas) {
+    private fun drawTriangle(canvas: Canvas) {
         val pathLeft = Path()
         pathLeft.moveTo(width/2-30f,height/2f)
         pathLeft.lineTo(width/2f,lampSpace+60f)
@@ -201,29 +218,38 @@ open class LuckPan: View {
         val cx = width/2f
         val cy = height/2f
         val radius = panSize /6f
+        //绘制橙黄圆
         canvas.drawCircle(cx,cy,radius,paintBg2)
         canvas.drawCircle(cx,cy,radius-4,paintBg)
         //绘制小灯泡
         val cxS = width/2f
         val cyS =  width.coerceAtMost(height) /2f-radius+8
-        val ratate = 360f/lampNum//灯泡之间的角度
-        //平分的份数绘制小圆点
+        //绘制小灯泡，平分的份数
         for (i in 0 until lampNum) {
             paintLamp.color = Color.parseColor("#ffffff")
             paintLamp.style = Paint.Style.FILL
             canvas.drawCircle(cxS,cyS,2f, paintLamp)
-            canvas.rotate(ratate, width / 2f, height / 2f)
+            canvas.rotate(lampAngle, width / 2f, height / 2f)
         }
+        //绘制白橙圆
         canvas.drawCircle(cx,cy,radius-14,paintLamp)
         canvas.drawCircle(cx,cy,radius-16,if (isTouchDown)paintBg else paintBg2)
+        //绘制中心文字
+        val str1 = "幸运"
+        val str2 = "转盘"
+        val textWidth = paintBtnText.measureText(str1)
+        val fm = paintBtnText.fontMetricsInt
+        val textHeight = fm.top -(fm.top-fm.ascent)-(fm.bottom-fm.descent)
+
+        canvas.drawText(str1,cx-textWidth/2,cy-14,paintBtnText)
+        canvas.drawText(str2,cx-textWidth/2,cy-textHeight,paintBtnText)
 
     }
 
     /**
      * 开始转动
-     * @param pos 如果 pos = -1 则随机，如果指定某个值，则转到某个指定区域
      */
-      fun startRotate() {
+    private fun startRotate() {
         luckPosition = (Math.random() *panNum).toInt()
 
         initAngle=0f
@@ -231,8 +257,8 @@ open class LuckPan: View {
         val angle = luckPosition*panRadius//中奖区域需要转动的角度
         val pointDiffRotate: Int = (270-panRadius).roundToInt()+randomJd-angle.toInt()//开始位置和指针位置的偏移角度
         val desRotate: Int = turnNum * 360+pointDiffRotate
-        val animtor = ValueAnimator.ofInt(initAngle.toInt(), desRotate)
-        animtor.apply {
+        val animator = ValueAnimator.ofInt(initAngle.toInt(), desRotate)
+        animator.apply {
             interpolator = DecelerateInterpolator(1.1f)
             duration = turnTime
             addUpdateListener { animation ->
@@ -273,8 +299,8 @@ open class LuckPan: View {
             }
 
             MotionEvent.ACTION_UP -> {//抬起了手指
-                    isTouchDown=false
-                    postInvalidate()
+                isTouchDown=false
+                postInvalidate()
             }
         }
         return super.onTouchEvent(event)
