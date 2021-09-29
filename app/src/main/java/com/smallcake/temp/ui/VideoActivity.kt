0d9efedd6bdaa97919d949ad897fffc67d618eb7
xiaoshubin <495303648@qq.com>
2021-09-29 16:06:16 +0800

    视频播放

--- app/src/main/java/com/smallcake/temp/ui/VideoActivity.kt
+++ app/src/main/java/com/smallcake/temp/ui/VideoActivity.kt
@@ -7,13 +7,20 @@ import android.media.MediaMetadataRetriever
 import android.net.Uri
 import android.os.Bundle
 import android.text.TextUtils
+import android.widget.ImageView
 import coil.load
 import coil.request.videoFrameMillis
+import com.bumptech.glide.Glide
 import com.hw.videoprocessor.VideoProcessor
 import com.luck.picture.lib.PictureSelector
 import com.luck.picture.lib.config.PictureMimeType
 import com.luck.picture.lib.entity.LocalMedia
 import com.luck.picture.lib.listener.OnResultCallbackListener
+import com.shuyu.gsyvideoplayer.GSYVideoManager
+import com.shuyu.gsyvideoplayer.builder.GSYVideoOptionBuilder
+import com.shuyu.gsyvideoplayer.listener.GSYSampleCallBack
+import com.shuyu.gsyvideoplayer.utils.Debuger
+import com.shuyu.gsyvideoplayer.utils.OrientationUtils
 import com.smallcake.smallutils.FileUtils
 import com.smallcake.smallutils.FormatUtils
 import com.smallcake.smallutils.SdUtils
@@ -32,14 +39,83 @@ import java.text.NumberFormat
  * 1.视频选择
  * 2.视频显示
  * 3.视频压缩
+ *
  */
 @SuppressLint("SetTextI18n")
 class VideoActivity : BaseBindActivity<ActivityVideoBinding>() {
-    private var videoPath =""
+    private var videoPath =""//本地视频
+    private val url = "http://9890.vod.myqcloud.com/9890_4e292f9a3dd011e6b4078980237cc3d3.f20.mp4"//网络视频
     override fun onCreate(savedInstanceState: Bundle?, bar: NavigationBar) {
         bar.setTitle("视频相关")
+        initVideoPlayer()
         onEvent()
     }
+
+    private fun initVideoPlayer() {
+        //1.设置url地址
+        bind.videoPlayer.setUp(url,true,"")
+        //2.设置封面
+        val img = ImageView(this)
+        Glide.with(img).load(url).into(img)
+        bind.videoPlayer.apply {
+            fullscreenButton.setOnClickListener{
+                bind.videoPlayer.startWindowFullscreen(this@VideoActivity,true,true)
+            }
+        }
+        //3.外部辅助的旋转，帮助全屏
+        val orientationUtils = OrientationUtils(this, bind.videoPlayer)
+        orientationUtils.isEnable = false
+        val gsyVideoOption = GSYVideoOptionBuilder()
+        gsyVideoOption.setThumbImageView(img)
+            .setIsTouchWiget(true)
+            .setRotateViewAuto(true)
+            .setLockLand(true)
+            .setAutoFullWithSize(true)
+            .setShowFullAnimation(true)
+            .setNeedLockFull(false)
+            .setUrl(url)
+            .setCacheWithPlay(true)
+            .setVideoTitle("")
+            .setVideoAllCallBack(object : GSYSampleCallBack() {
+                override fun onPrepared(url: String, vararg objects: Any) {
+                    super.onPrepared(url, *objects)
+                    //开始播放了才能旋转和全屏
+                    orientationUtils.isEnable = true
+                }
+                override fun onQuitFullscreen(url: String, vararg objects: Any) {
+                    super.onQuitFullscreen(url, *objects)
+                    Debuger.printfError("***** onQuitFullscreen **** " + objects[0]) //title
+                    Debuger.printfError("***** onQuitFullscreen **** " + objects[1]) //当前非全屏player
+                    orientationUtils.backToProtVideo()
+                }
+            }).setLockClickListener { view, lock ->
+                orientationUtils.isEnable = !lock
+            }.build(bind.videoPlayer)
+    }
+    override fun onBackPressed() {
+        if (GSYVideoManager.backFromWindowFull(this)) {
+            return
+        }
+        super.onBackPressed()
+    }
+    override fun onPause() {
+        super.onPause()
+        GSYVideoManager.onPause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        GSYVideoManager.onResume()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        GSYVideoManager.releaseAllVideos()
+    }
+
+
+
+
 
     private fun onEvent() {
         bind.btnSelect.setOnClickListener{
